c94f3f72b9e56bbc90c90b174de6eeaa975be0d2
ggjj321 <10370324lsjh@gmail.com>
2022-08-05 16:38:19 +0800

    build: result page animation

--- foot-ball-detect-app/lib/views/screens/result_page.dart
+++ foot-ball-detect-app/lib/views/screens/result_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import '../widgets/barchart.dart';
 import '../widgets/wave_clip.dart';
 import '../widgets/goal_painter.dart';
@@ -43,44 +44,36 @@ class ResultPage extends StatelessWidget {
               ),
             ),
           )),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            textAlign: TextAlign.center,
-            'Where your ball shoot at',
-            style: TextStyle(
-                fontSize: 20,
-                foreground: Paint()
-                  ..shader = getLinearGradient(
-                      Colors.purple, Colors.blue, 0, 350
-                  )),
-          ),
-          Container(
-            height: screenHeight * 0.2,
-            width: screenWidth * 0.9,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: CustomPaint(
-              foregroundPainter: GoalPainter(0.4, 0.2),
-            ),
-          ),
-          Row(
+      body: SingleChildScrollView(
+
+        child: AnimationLimiter(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
+            children: AnimationConfiguration.toStaggeredList(
+              duration: const Duration(milliseconds: 375),
+              childAnimationBuilder: (widget) => SlideAnimation(
+                horizontalOffset: 50.0,
+                child: FadeInAnimation(
+                  child: widget,
+                ),
+              ),
+              children: [
+                Text(
+                  textAlign: TextAlign.center,
+                  'Where your ball shoot at',
+                  style: TextStyle(
+                      fontSize: 20,
+                      foreground: Paint()
+                        ..shader = getLinearGradient(
+                            Colors.purple, Colors.blue, 0, 350
+                        )),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Container(
                   height: screenHeight * 0.2,
-                  width: screenWidth * 0.40,
+                  width: screenWidth * 0.9,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.white,
@@ -93,75 +86,106 @@ class ResultPage extends StatelessWidget {
                       ),
                     ],
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'target difference',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20,
-                            foreground: Paint()
-                              ..shader = getLinearGradient(
-                                  Colors.purple, Colors.blue, 0, 150
-                              )),
-                      ),
-                      Text(
-                        textAlign: TextAlign.center,
-                        '5 M',
-                        style: TextStyle(
-                            fontSize: 40,
-                            foreground: Paint()
-                              ..shader = getLinearGradient(
-                                  Colors.purple, Colors.blue, 0, 150
-                              )),
-                      ),
-                    ],
-                  )),
-              Container(
-                  height: screenHeight * 0.2,
-                  width: screenWidth * 0.40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
+                  child: CustomPaint(
+                    foregroundPainter: GoalPainter(0.4, 0.2),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Your score',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20,
-                            foreground: Paint()
-                              ..shader = getLinearGradient(
-                                  Colors.purple, Colors.blue, 200, 450
-                              )),
-                      ),
-                      Text(
-                        textAlign: TextAlign.center,
-                        '20',
-                        style: TextStyle(
-                            fontSize: 40,
-                            foreground: Paint()
-                              ..shader = getLinearGradient(
-                                  Colors.purple, Colors.blue, 200, 400
-                              )),
-                      ),
-                    ],
-                  ))
-            ],
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        height: screenHeight * 0.2,
+                        width: screenWidth * 0.40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'target difference',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  foreground: Paint()
+                                    ..shader = getLinearGradient(
+                                        Colors.purple, Colors.blue, 0, 150
+                                    )),
+                            ),
+                            Text(
+                              textAlign: TextAlign.center,
+                              '5 M',
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  foreground: Paint()
+                                    ..shader = getLinearGradient(
+                                        Colors.purple, Colors.blue, 0, 150
+                                    )),
+                            ),
+                          ],
+                        )),
+                    Container(
+                        height: screenHeight * 0.2,
+                        width: screenWidth * 0.40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Your score',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  foreground: Paint()
+                                    ..shader = getLinearGradient(
+                                        Colors.purple, Colors.blue, 200, 450
+                                    )),
+                            ),
+                            Text(
+                              textAlign: TextAlign.center,
+                              '20',
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  foreground: Paint()
+                                    ..shader = getLinearGradient(
+                                        Colors.purple, Colors.blue, 200, 400
+                                    )),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
+                SizedBox(
+                  height: screenHeight * 0.05,
+                ),
+                const BarChartSample3(),
+              ]
+            ),
           ),
-          const BarChartSample3(),
-        ],
+        ),
       ),
     );
   }
